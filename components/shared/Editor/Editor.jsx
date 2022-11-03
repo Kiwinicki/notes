55c026c35ec35ccacefca03a3aeddf7f389cf092
Kiwinicki <szefek24@gmail.com>
2022-11-03 11:48:33 +0100

    added resizable editor

--- components/shared/Editor/Editor.jsx
+++ components/shared/Editor/Editor.jsx
@@ -1,3 +1,4 @@
+import { useState, useRef } from 'react';
 import styles from './Editor.module.scss';
 import { useApp, userTypes } from '../../../store/useApp';
 import MonacoEditor from '@monaco-editor/react';
@@ -10,19 +11,38 @@ export const Editor = ({ saveHandler }) => {
 
 	const content = useNoteStore((state) => state.content);
 
+	const editorRef = useRef();
+	const [width, setWidth] = useState(200);
+
+	const [initialPos, setInitialPos] = useState(null);
+	const [initialSize, setInitialSize] = useState(null);
+
+	const initial = (e) => {
+		setInitialPos(e.clientX);
+		setInitialSize(editorRef.current.offsetWidth);
+	};
+
+	const resize = (e) => {
+		setWidth(parseInt(initialSize) + parseInt(e.clientX - initialPos + 'px'));
+	};
+
 	return (
 		<div className={styles.noteContainer}>
 			<EditorNavBar saveHandler={saveHandler} />
 			<div className={styles.contentContainer}>
 				{data.userType === userTypes.admin && (
 					<>
-						<div>
+						<div
+							className={styles.editorContainer}
+							style={{ width }}
+							ref={editorRef}
+						>
 							<MonacoEditor
 								defaultLanguage="markdown"
 								defaultValue={content}
 								value={content}
 								theme="vs-dark"
-								className={styles.editor}
+								// className={styles.editor}
 								options={{
 									minimap: { enabled: false },
 									wordWrap: 'on',
@@ -31,8 +51,12 @@ export const Editor = ({ saveHandler }) => {
 								onChange={(val) => setValues({ content: val })}
 							/>
 						</div>
-						{/* TODO: resizing editor  */}
-						<div className={styles.resizeBar} />
+						<div
+							className={styles.resizeBar}
+							draggable
+							onDragStart={initial}
+							onDrag={resize}
+						/>
 					</>
 				)}
 				<MDXOutput />
